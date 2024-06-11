7dea4adcf7cffa5a4e74dc2c8d17f55e83182cee
Kristian Dalland <kdalland@buffalo.edu>
2024-06-11 10:28:13 -0400

    test

--- UItest.py
+++ UItest.py
@@ -4,28 +4,35 @@ import numpy as np
 
 class MouseTracker:
     def __init__(self):
-        
+
+        # Figure stuff
         fig, ax = plt.subplots() 
         plt.xlim(0,1)
         plt.ylim(0,1) 
-        fig.subplots_adjust(bottom=0.2)
-        button_ax = plt.axes([0.8, 0.05, 0.1, 0.075]) 
-        button = Button(button_ax, 'boooton')
-        button.on_clicked(self.on_button_clicked)
-        self.fig=fig
+        self.fig = fig
         self.ax = ax
-        self.fig = ax.figure
-        self.pressed = False
+        
+        # Button stuff
+        fig.subplots_adjust(bottom=0.2)
+        fig.subplots_adjust(left=0.2)
+        booted_ax = plt.axes([0.8, 0.05, 0.1, 0.075]) 
+        booted = Button(booted_ax, 'boooton')
+        booted.on_clicked(self.booted)
+
+        # Coordinate ledger
         self.reset_sketch()
+        
+        # Mouse tracking
         self.cidpress = self.fig.canvas.mpl_connect('button_press_event', self.on_press)
         self.cidrelease = self.fig.canvas.mpl_connect('button_release_event', self.on_release)
         self.cidmotion = self.fig.canvas.mpl_connect('motion_notify_event', self.on_motion)
-        self.line=None
-        self.button=button
+        self.pressed = False
+            
         plt.show()
+        
     
-    def on_button_clicked(self, event):
-        print("Button clicked!")
+    def booted(self,event):
+        print("Booted")
         for line in self.ax.lines:
             line.remove()
         for scatter in self.ax.collections:
@@ -33,26 +40,26 @@ class MouseTracker:
         self.reset_sketch()
         self.fig.canvas.draw()
 
-
-
-  
-
     def reset_sketch(self):
-        self.x = np.array([])
-        self.y = np.array([])
-
-   
+        self.x = []
+        self.y = []
+        self.dots=[]
 
     def on_press(self, event):
         if event.inaxes != self.ax:
             return
         if event.button == 1:  # Left mouse button
+            if self.pressed==False:
+                self.dots.append(1)
             self.pressed = True
-            self.x=np.append(self.x,[event.xdata])
-            self.y=np.append(self.y,[event.ydata])
-
+            self.x.append([event.xdata])
+            self.y.append([event.ydata])
+            self.ax.scatter(self.x[-1],self.y[-1])
         elif event.button == 3:
             self.ax.lines[-1].remove()
+            for _ in range(self.dots[-1]):
+                self.ax.collections[-1].remove()
+            del self.dots[-1],self.x[-1],self.y[-1]
             self.fig.canvas.draw()
 
     def on_release(self, event):
@@ -60,20 +67,17 @@ class MouseTracker:
             return
         if event.button == 1:  # Left mouse button
             self.pressed = False
-            self.do_stuff()
-            self.reset_sketch()
- 
-    def do_stuff(self):
-        self.line=self.ax.plot(self.x,self.y)
-        self.fig.canvas.draw()
-
+            self.ax.plot(self.x,self.y)
+            self.fig.canvas.draw()
+    
     def on_motion(self, event):
         if event.inaxes != self.ax:
             return
         if self.pressed:
-            self.x=np.append(self.x,[event.xdata])
-            self.y=np.append(self.y,[event.ydata])
+            self.x[-1].append(event.xdata)
+            self.y[-1].append(event.ydata)
             self.ax.scatter(self.x[-1],self.y[-1])
+            self.dots[-1]+=1
             self.fig.canvas.draw()
     
     
